b52d452bcded64bb1eb8f1dd6ca266342b01c15a
EKorenovsky <ekorenovsky@gmail.com>
2019-12-03 00:21:40 +0300

    поменял на изменение display

--- JS_7/js/script3.js
+++ JS_7/js/script3.js
@@ -1,11 +1,16 @@
 function changeLights() {
     let counter = 0;
-    let elem = document.getElementById("light");
     let koeff = 1;
 
     function changer() {
         counter = koeff + counter;
-        elem.src = `img/${counter}.jpg`;
+        for (let i = 0; i <= 3; i++) {
+            if (i === counter) {
+                document.getElementById(`light${i}`).style = "display:block;";
+            } else {
+                document.getElementById(`light${i}`).style = "display:none;";
+            }
+        }
 
         if (counter === 3)
             koeff = -1;
@@ -18,5 +23,7 @@ function changeLights() {
 
 }
 
+
 let fn = changeLights();
 setInterval(fn, 2000);
+//fn();
